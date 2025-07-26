2bc1c52a02dc9ec2a0ab4cc025f483e984d29d36
Kaiyrkhan <kairolla222@gmail.com>
2025-07-26 13:41:45 +0500

    create db connection

--- fem_project/internal/app/app.go
+++ fem_project/internal/app/app.go
@@ -1,30 +1,37 @@
 package app
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/sogeking7/femProject/internal/api"
+	"github.com/sogeking7/femProject/internal/store"
 )
 
 type Application struct {
 	Logger         *log.Logger
 	WorkoutHandler *api.WorkoutHandler
+	DB             *sql.DB
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// our stores will go here
-
+	pgDB, err := store.Open()
+	if err != nil {
+		return nil, err
+	}
 	// our handlers will go here
 	workoutHandler := api.NewWorkoutHandler()
 
 	app := &Application{
 		Logger:         logger,
 		WorkoutHandler: workoutHandler,
+		DB:             pgDB,
 	}
 
 	return app, nil
